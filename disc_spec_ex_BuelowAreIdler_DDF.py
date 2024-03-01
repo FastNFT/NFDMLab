24da5eaeecd39987e121a659cfaf54b921d99662
Sander Wahls <wahls@kit.edu>
2024-03-01 15:17:37 +0100

    removed some files that appear to be left-overs from testing

--- disc_spec_ex_BuelowAreIdler_DDF.py
+++
@@ -1,8 +0,0 @@
-import Examples
-ex = Examples.BajajEtAl2020_multi_soliton()
-tx_data, rx_data = ex.run(5) # 5 is the number of bursts
-ex.evaluate_results(tx_data, rx_data)
-from QualityAssessment import BitErrorRatio
-ber = BitErrorRatio(ex.constellation)
-ber_value, n_err, n_bits = ber.compute(tx_data["symbols"], rx_data["symbols"])
-print("The bit error ratio is", ber_value)
